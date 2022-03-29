f93b1bbcb4de22c6322fc56607c4c7d31148472e
Daniel Park <danielpark1140@gmail.com>
2022-03-29 14:42:56 -0400

    react-hooks complete

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,31 +3,54 @@
 
 import * as React from 'react'
 
+
+const useLocalStorageState = (key, defaultValue = '', {
+    serialize = JSON.stringify,
+    deserialize = JSON.parse
+} = {}) => {
+    const [state, setState] = React.useState(
+        () => {
+            const valueInLocalStorage = window.localStorage.getItem(key);
+            if (valueInLocalStorage) {
+                return deserialize(valueInLocalStorage);
+            }
+            return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
+        },
+    )
+    // support migrating keys over
+    const prevKeyRef = React.useRef(key)
+    React.useEffect(() => {
+        const prevKey = prevKeyRef.current
+        if (prevKey !== key) {
+            window.localStorage.removeItem(key)
+        }
+        prevKeyRef.current = key
+        window.localStorage.setItem(key, serialize(state))
+    }, [key, state, serialize])
+
+    return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+    const [name, setName] = useLocalStorageState('name', initialName)
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+    function handleChange(event) {
+        setName(event.target.value)
+    }
 
-  function handleChange(event) {
-    setName(event.target.value)
-  }
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input value={name} onChange={handleChange} id="name"/>
+            </form>
+            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+        </div>
+    )
 }
 
 function App() {
-  return <Greeting />
+    return <Greeting/>
 }
 
 export default App
